11e4853a57a8a32880ee5e001c529ae53d69b957
Danilo Lemes <daniloleemes@hotmail.com>
2020-12-04 15:29:32 +0000

    add dynamic character cell

--- Assets/Scenes/CharSelection/CharSelectionController.cs
+++ Assets/Scenes/CharSelection/CharSelectionController.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class CharSelectionController : MonoBehaviour
-{
+public class CharSelectionController : MonoBehaviour {
+
+    public GridLayoutGroup GridLayout;
+    public GameObject charSelectionItem;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        var charInfo = Core.NetworkClient.State.CurrentCharactersInfo;
+        for(var i = 0; i < charInfo.MaxSlots; i++) {
+            var item = Instantiate(charSelectionItem);
+            item.transform.parent = GridLayout.transform;
+
+            var controller = item.GetComponent<CharacterCellController>();
+            if(i < charInfo.Chars.Length) {
+                controller.BindData(charInfo.Chars[i]);
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
---
+++ Assets/Scenes/CharSelection/CharacterCellController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterCellController : MonoBehaviour {
+
+    public Text characterName;
+    private CharacterData data;
+
+    public void BindData(CharacterData data) {
+        this.data = data;
+
+        this.characterName.text = data.Name;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
