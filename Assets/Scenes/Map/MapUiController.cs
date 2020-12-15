d1125b5cfea5a3c7ac70e4f56643221ef4bc1437
Danilo Menezes <danilo.menezes@sky.uk>
2020-12-15 21:26:58 +0000

    make char

---
+++ Assets/Scenes/Map/MapUiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapUiController : MonoBehaviour {
+
+    public InputField chatField;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            SendChatMessage();
+        }
+    }
+
+    public void SendChatMessage() {
+        var message = chatField.text;
+        if (message.Length == 0) return;
+
+        new CZ.REQUEST_CHAT(message).Send();
+    }
+}
