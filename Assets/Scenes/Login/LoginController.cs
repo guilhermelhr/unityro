399984d5798513208b71d66f03d96fe682891bde
Jason L <jasontated@hotmail.com>
2021-06-13 16:58:29 -0400

    Small QOL Changes to UI
    Made it possible to navigate login/server select with tab.
    Buttons now properly display when "selected".
    Clicking on the server you want to select actually selects it now.

--- Assets/Scenes/Login/LoginController.cs
+++ Assets/Scenes/Login/LoginController.cs
@@ -1,17 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class LoginController : MonoBehaviour {
 
     public InputField usernameField;
     public InputField passwordField;
 
+    private EventSystem system;
+
     void Start() {
         Core.NetworkClient.ChangeServer("127.0.0.1", 6900);
         Core.NetworkClient.HookPacket(AC.ACCEPT_LOGIN3.HEADER, this.OnLoginResponse);
 
         usernameField.text = "danilo3";
+
+        system = EventSystem.current;
+    }
+
+    void Update() {
+        TabBehaviour();
+    }
+
+    private void TabBehaviour() {
+        if (system.currentSelectedGameObject == null || !Input.GetKeyDown(KeyCode.Tab))
+            return;
+
+        Selectable current = system.currentSelectedGameObject.GetComponent<Selectable>();
+        if (current == null)
+            return;
+ 
+        bool up = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        Selectable next = up ? current.FindSelectableOnUp() : current.FindSelectableOnDown();
+        next = current == next || next == null ? Selectable.allSelectablesArray[0] : next;
+        system.SetSelectedGameObject(next.gameObject);
     }
 
     public void OnLoginClicked() {
