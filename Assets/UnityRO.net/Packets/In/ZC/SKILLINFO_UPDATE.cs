083f60549e52ba106d4794c02146204e3103b3d8
Danilo Lemes <daniloleemes@hotmail.com>
2021-08-21 14:38:58 +0100

    ZC_SKILLINFO_UPDATE

---
+++ Assets/UnityRO.net/Packets/In/ZC/SKILLINFO_UPDATE.cs
@@ -0,0 +1,25 @@
+﻿using ROIO.Utils;
+
+public partial class ZC {
+
+    [PacketHandler(HEADER, "ZC_SKILLINFO_UPDATE", SIZE)]
+    public class SKILLINFO_UPDATE : InPacket {
+
+        public const PacketHeader HEADER = PacketHeader.ZC_SKILLINFO_UPDATE;
+        public const int SIZE = 11;
+
+        public short SkillId;
+        public short SkillLevel;
+        public short SpCost;
+        public short AttackRange;
+        public bool IsUpgradeable;
+
+        public void Read(MemoryStreamReader br, int size) {
+            SkillId = br.ReadShort();
+            SkillLevel = br.ReadShort();
+            SpCost = br.ReadShort();
+            AttackRange = br.ReadShort();
+            IsUpgradeable = br.ReadByte() == 1;
+        }
+    }
+}
