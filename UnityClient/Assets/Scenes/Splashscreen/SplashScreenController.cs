221949292f7bde6ae1eba2dd48bb6442623777ce
Danilo Lemes <daniloleemes@hotmail.com>
2022-09-22 19:48:18 +0100

    Safe divide, query for download size before starting everything and add a progress bar

--- UnityClient/Assets/Scenes/Splashscreen/SplashScreenController.cs
+++ UnityClient/Assets/Scenes/Splashscreen/SplashScreenController.cs
@@ -1,12 +1,14 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using ROIO;
+using ROIO.Utils;
 using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SplashScreenController : MonoBehaviour {
 
@@ -16,36 +18,58 @@ public class SplashScreenController : MonoBehaviour {
     [SerializeField]
     private TextMeshProUGUI labelText;
 
+    [SerializeField]
+    private TextMeshProUGUI DownloadSizeText;
+
+    [SerializeField]
+    private Slider Slider;
+
     void Start() {
+        StartCoroutine(Initialize());
+    }
+
+    private IEnumerator Initialize() {
+        labelText.text = "Checking for updates...";
+        yield return Addressables.InitializeAsync();
+        yield return new WaitForSeconds(1f);
+
         StartCoroutine(PrefetchAssets());
     }
 
     private IEnumerator PrefetchAssets() {
 #if !UNITY_EDITOR
+        var downloadSize = Addressables.GetDownloadSizeAsync(LabelsToPrefetch).WaitForCompletion();
+
+        if (downloadSize <= 0) {
+            yield return FetchConfigs();
+        }
+
         foreach (var label in LabelsToPrefetch) {
             var handle = Addressables.DownloadDependenciesAsync(label, true);
 
             while(!handle.IsDone) {
                 var downloadStatus = handle.GetDownloadStatus();
-                var downloadedMbs = downloadStatus.DownloadedBytes / 1024 / 1024;
-                var totalMbs = downloadStatus.TotalBytes / 1024 / 1024;
-                var progress = downloadedMbs / (totalMbs > 0 ? totalMbs : 1) * 100;
+                var downloadedMbs = downloadStatus.DownloadedBytes / 1024f / 1024f;
+                var totalMbs = (downloadStatus.TotalBytes / 1024f / 1024f);
 
-                var text = $"Downloading {label.labelString}\n{downloadedMbs}MB / {totalMbs}MB\n{progress}%";
+                var progress = Conversions.SafeDivide(downloadedMbs, totalMbs);
+
+                var text = $"Downloading {label.labelString}";
                 labelText.text = text;
+                DownloadSizeText.text = $"{downloadedMbs}MB / {totalMbs}MB";
+                Slider.value = progress;
+
                 yield return null;
             }
 
             yield return handle;
         }
 #endif
-        yield return Addressables.InitializeAsync();
-        yield return new WaitForSeconds(1f);
-
-        StartCoroutine(FetchConfigs());
+        yield return FetchConfigs();
     }
 
     private IEnumerator FetchConfigs() {
+        labelText.text = "Fetching remote configuration...";
         var localRequest = Addressables.LoadAssetAsync<TextAsset>("LocalConfigs.json.txt");
         yield return localRequest;
 
