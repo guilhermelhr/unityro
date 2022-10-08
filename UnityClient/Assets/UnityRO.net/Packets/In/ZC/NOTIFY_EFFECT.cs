382cee9b6b99a3e4d538697670786300e3145f24
Danilo Lemes <daniloleemes@hotmail.com>
2022-10-08 17:57:15 +0100

    ZC_NOTIFY_EFFECT & ZC_USE_SKILL

---
+++ UnityClient/Assets/UnityRO.net/Packets/In/ZC/NOTIFY_EFFECT.cs
@@ -0,0 +1,32 @@
+﻿using ROIO.Utils;
+
+public partial class ZC {
+
+    [PacketHandler(HEADER, "ZC_NOTIFY_EFFECT", SIZE)]
+    public class NOTIFY_EFFECT : InPacket {
+        public const PacketHeader HEADER = PacketHeader.ZC_NOTIFY_EFFECT;
+        public const int SIZE = 10;
+
+        public PacketHeader Header => HEADER;
+
+        public int sourceAID;
+
+        /// effect id:
+        ///     0 = base level up
+        ///     1 = job level up
+        ///     2 = refine failure
+        ///     3 = refine success
+        ///     4 = game over
+        ///     5 = pharmacy success
+        ///     6 = pharmacy failure
+        ///     7 = base level up (super novice)
+        ///     8 = job level up (super novice)
+        ///     9 = base level up (taekwon)
+        public int effectID;
+
+        public void Read(MemoryStreamReader br, int size) {
+            sourceAID = br.ReadInt();
+            effectID = br.ReadInt();
+        }
+    }
+}
